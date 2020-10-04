be1861e3b75977f5adb5ff6f17f44d081f401bb8
Yeghon <haronyegon90@gmail.com>
2020-10-04 10:14:00 +0300

    Add the different app tabs

--- app/src/main/java/com/yeghon/myfarmapp/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/yeghon/myfarmapp/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.yeghon.myfarmapp.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_crops, R.string.tab_livestock, R.string.tab_market, R.string.tab_other, R.string.tab_news};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -40,7 +40,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        return TAB_TITLES.length;
     }
 }
